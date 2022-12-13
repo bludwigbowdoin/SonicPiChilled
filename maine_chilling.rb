5909ff46202b51faa67026e7240992c539564310
bludwigbowdoin <bludwig@bowdoin.edu>
2022-12-13 10:12:13 -0500

    restoring repo

--- maine_chilling.rb
+++ maine_chilling.rb
@@ -1,4 +1,6 @@
 ##| Bjorn Ludwig
+##| CSCI 3725
+##| M8: Hakuna Fermata
 
 
 quarter = 1
@@ -6,75 +8,100 @@ eighth = 0.5
 sixteenth = 0.25
 triplet = 0.33333333333333333
 
-##| use_synth :prophet
-##| use_synth :dsaw
-##| use_synth :fm
-##| use_synth :tb303
-##| use_synth :pulse
+amp_low = 0.4
+amp_med = 0.5
+amp_high = 0.6
+
+slow_attack = 1
+
+
+##| NOTE: The following six chord options sound nice in progression
+
+##| main_chord = chord(:c4, :minor7)
+##| main_chord_8va = chord(:c5, :minor7)
+
+##| main_chord = chord(:f4, :m7)
+##| main_chord_8va = chord(:f5, :m7)
+
+##| main_chord = chord(:c4, :m)
+##| main_chord_8va = chord(:c5, :m)
+
+##| main_chord = chord(:ab4, :maj11)
+##| main_chord_8va = chord(:ab4, :maj11)
+
+##| main_chord = chord(:g4, :major7)
+##| main_chord_8va = chord(:g4, :major7)
+
+main_chord = chord(:c4, :minor7)
+main_chord_8va = chord(:c4, :minor7)
 
 
 
+##| NOTE: The following are more rambunctious chords (not for the faint of heart)
+##| main_chord = chord(:g4, :dom7)
+##| main_chord_8va = chord(:g5, :dom7)
 
+##| main_chord = chord(:d4, :dom7)
+##| main_chord_8va = chord(:d5, :dom7)
 
-##| define :intro do
-##|   use_synth :saw
-##|   my_note = chord(:c5, :minor7).choose
-##|   play my_note, release: 2*quarter
-##|   sleep quarter
-##|   my_note = chord(:c5, :sus4).choose
-##|   play my_note, release: 2*quarter
-##|   sleep quarter
-##| end
+##| main_chord = chord(:f4, :dom7)
+##| main_chord_8va = chord(:f5, :dom7)
 
 
 
-##| intro
-
+sample "/Users/bjornludwig/Desktop/CompCreat/SonicPiChilled/ocean-wave.wav", rate: -0.66
 
 
 live_loop :first do
+  
   use_synth :saw
   
-  my_note = chord(:c5, :minor7).choose
-  amp_level = [1,2].choose
+  my_note = main_chord_8va.choose
+  amp_level = [amp_low, amp_med].choose
   
-  play my_note, amp: amp_level, release: 2*quarter
-  sleep [quarter, 2*quarter, 3*quarter].choose
+  play my_note, amp: amp_level, attack: slow_attack, release: 3*quarter - slow_attack
+  sleep [quarter, 2*quarter].choose
   
-  my_note = chord(:c4, :minor7).choose
-  amp_level = [1,2].choose
+  my_note = main_chord
+  amp_level = [amp_med, amp_high].choose
   
-  play my_note, amp: amp_level, release: 2*quarter
+  play my_note, amp: amp_level, attack: slow_attack, release: 3*quarter - slow_attack
   sleep [quarter, 2*quarter, 3*quarter].choose
 end
 
 
 live_loop :second do
+  ##| stop
+  
   use_synth :tb303
-  my_note = chord(:c5, :minor7).choose
-  amp_level = [1,2].choose
+  my_note = main_chord_8va.choose
+  amp_level = [amp_low,amp_med].choose
   
   play my_note, amp: amp_level, release: eighth
-  sleep eighth
+  sleep [eighth, 2*eighth, 3*eighth].choose
 end
 
 
 live_loop :third do
-  use_synth :tb303
-  my_note = chord(:c5, :minor7).choose
-  amp_level = [1,2].choose
+  ##| stop
   
-  play my_note, amp: amp_level, release: 2*triplet
+  use_synth :tb303
+  my_note = main_chord_8va.choose
+  amp_level = [amp_low,amp_med].choose
+  
+  play my_note, amp: amp_level, release: 4*triplet
   sleep 2*triplet
 end
 
 
 live_loop :fourth do
-  use_synth :tb303
-  my_note = chord(:c5, :minor7).choose
-  amp_level = [1,2].choose
+  ##| stop
   
-  play my_note, amp: amp_level, release: triplet
+  use_synth :tb303
+  my_note = main_chord_8va.choose
+  amp_level = [amp_low,amp_med].choose
+  
+  play my_note, amp: amp_level, release: 2*triplet
   sleep triplet
 end
 
